f8a692036e816797cee802072adf9acb02e2323a
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-05-06 11:34:35 +0100

    new commit

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -7,9 +7,9 @@ import "./App.css";
 
 function App() {
   const products = [
-    { id: 1, name: "Product 1", price: 10 },
-    { id: 2, name: "Product 1", price: 20 },
-    { id: 3, name: "Product 1", price: 30 },
+    { id: 1, name: "Product 1", qty: 1, price: 10 },
+    { id: 2, name: "Product 1", qty: 1, price: 20 },
+    { id: 3, name: "Product 1", qty: 1, price: 30 },
   ];
 
   // const [resourceType, setResourceType]= useState('user')
